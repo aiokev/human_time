3c2c32834fff607653e0a7374f67c43be8664092
Kev <121695593+aiokev@users.noreply.github.com>
2023-11-04 18:59:50 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+desc = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="humanize-time",
+    version="2.0.0",
+    description="A module with functions to humanize time/duration.",
+    long_description=desc,
+    long_description_content_type="text/markdown",
+    author="aiokev",
+    url="https://github.com/aiokev/human_time",
+    python_requires=">=3.6",
+    packages=find_packages(include=["human_time", "human_time.*"]),
+)
