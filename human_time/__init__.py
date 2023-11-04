cac4b37c6e9f8972fdd5dcf70d77fad66631ca43
Kev <121695593+aiokev@users.noreply.github.com>
2023-11-04 19:15:20 +0100

    Update __init__.py

--- human_time/__init__.py
+++ human_time/__init__.py
@@ -2,7 +2,7 @@ class humantime():
     def __init__(self):
         super().__init__()
 
-    def timedelta(seconds: int = 0, short: bool = None):
+    def humanize(seconds: int = 0, short: bool = None):
         time = seconds
         seconds_to_minute   = 60
         seconds_to_hour     = 60 * seconds_to_minute
